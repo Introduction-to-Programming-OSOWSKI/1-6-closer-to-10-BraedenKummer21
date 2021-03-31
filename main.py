e25d37c3cdbf33d0a092356b834586b447afdbfa
Kummer Braeden <813664@stma.k12.mn.us>
2021-03-31 10:12:03 -0500

    close10 try 3

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #WRITE YOUR CODE IN THIS FILE
 def close10(x,y):
 
-    if abs(x+10) < abs(y-10):
-        print(x)
+    if abs(x-10) < abs(y-10):
+        return x
 
-    elif abs(x+10) > abs(y-10):
-        print(y) 
+    elif abs(x-10) > abs(y-10):
+        return y 
 
     else: 
         print("0")
